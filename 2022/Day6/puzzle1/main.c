ce215e8ff53a41cebc6e013fc66e5a40fd9d83ae
root2 <arthur.lassausaie@epitech.eu>
2023-12-01 23:39:30 +0100

    {ADD}
    Adding day6 puzzles

--- 2022/Day6/puzzle1/main.c
+++ 2022/Day6/puzzle1/main.c
@@ -6,50 +6,6 @@
 #include <stddef.h>
 #include <string.h>
 
-char **load_2d_arr_from_file(char *filecontent)
-{
-    int num_line = 0;
-    int num_char = 0;
-    int index = 0;
-    int temp_index = 0;
-    int result_index = 0;
-    int temp_result_index = 0;
-    while (filecontent[index] != '\0') {
-        if (filecontent[index] == '\n')
-            num_line += 1;
-        index += 1;
-    }
-    char **result = malloc(sizeof(char *) * (num_line + 2));
-    index = 0;
-    while (filecontent[index] != '\0') {
-        temp_index = index;
-        num_char = 0;
-        while (filecontent[temp_index] != '\0') {
-            if (filecontent[temp_index] == '\n')
-                break;
-            temp_index++;
-            num_char++;
-        }
-        result[result_index] = malloc(sizeof(char) * (num_char + 1));
-        result[result_index][0] = '\0';
-        temp_result_index = 0;
-        while (filecontent[index] != '\0') {
-            if (filecontent[index] == '\n') {
-                index++;
-                break;
-            }
-            result[result_index][temp_result_index] = filecontent[index];
-            temp_result_index++;
-            index++;
-        }
-        result[result_index][num_char] = '\0';
-        result_index++;
-    }
-    result[num_line + 1] = NULL;
-    free(filecontent);
-    return result;
-}
-
 char *load_file_in_mem(void)
 {
     struct stat s;
@@ -65,15 +21,30 @@ char *load_file_in_mem(void)
     return content;
 }
 
+int is_same_char(int len, int i, char const *filecontent)
+{
+    for (int j = 0; j < len - 1; j++) {
+        for (int k = j + 1; k < len; k++) {
+            if (filecontent[i + j] == filecontent[i + k]) {
+                return 0;
+            }
+        }
+    }
+    return i + len;
+}
+
 int main(void)
 {
+    int len = 14;
     int result = 0;
-    char **filecontent = load_2d_arr_from_file(load_file_in_mem());
-    for (int i = 0; filecontent[i] != NULL; i++) {
+    char *filecontent = load_file_in_mem();
+    for (int i = 0; filecontent[i + len] != '\0'; i++) {
+        if (is_same_char(len, i, filecontent) != 0) {
+            result = is_same_char(len, i, filecontent);
+            break;
+        }
     }
     printf("%d\n", result);
-    for (int i = 0; filecontent[i] != NULL; i++)
-        free(filecontent[i]);
     free(filecontent);
     return 0;
 }
