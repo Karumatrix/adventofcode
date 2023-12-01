8df33fca3c41bdc315bdb76e44a2275752dc9924
root2 <arthur.lassausaie@epitech.eu>
2023-12-01 22:45:09 +0100

    {ADD}
    Adding second puzzle of 5th day

--- 2022/Day5/puzzle2/main.c
+++ 2022/Day5/puzzle2/main.c
@@ -6,6 +6,11 @@
 #include <stddef.h>
 #include <string.h>
 
+typedef struct crates_s {
+    char crate_name;
+    struct crates_s *next;
+} crates_t;
+
 char **load_2d_arr_from_file(char *filecontent)
 {
     int num_line = 0;
@@ -65,13 +70,126 @@ char *load_file_in_mem(void)
     return content;
 }
 
-int main(void)
+crates_t *add_end_node(crates_t *stacks, char crate, int stack)
+{
+    crates_t *result = malloc(sizeof(crates_t));
+    crates_t *cursor = stacks;
+    result->crate_name = crate;
+    result->next = NULL;
+    if (stacks == NULL) {
+        return result;
+    }
+    while (cursor->next != NULL)
+        cursor = cursor->next;
+    cursor->next = result;
+    return stacks;
+}
+
+crates_t **init_map(char **filecontent)
+{
+    crates_t **result = NULL;
+    int num_stack = 0;
+    for (int i = 0; filecontent[7][i] != '\0'; i++)
+        if (filecontent[7][i] == '[')
+            num_stack += 1;
+    result = malloc(sizeof(crates_t *) * (num_stack + 1));
+    result[num_stack] = NULL;
+    int k = 1;
+    for (int i = 0; i < num_stack; i++) {
+        result[i] = NULL;
+        for (int j = 0; strcmp(filecontent[j + 1], "\0") != 0; j++) {
+            if (filecontent[j][k] != ' ')
+                result[i] = add_end_node(result[i], filecontent[j][k], i + 1);
+        }
+        k += 4;
+    }
+    return result;
+}
+
+void print_stacks(crates_t **orga, int len)
+{
+    for (int i = 0; i < len; i++) {
+        crates_t *cursor = orga[i];
+        printf("%d : ", i + 1);
+        while (cursor != NULL) {
+            printf("[%c]", cursor->crate_name);
+            cursor = cursor->next;
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void free_stack(crates_t *stack)
+{
+    if (stack->next == NULL) {
+        free(stack);
+        return;
+    }
+    free_stack(stack->next);
+    free(stack);
+    return;
+}
+
+int get_value(char const *line, int index)
 {
     int result = 0;
-    char **filecontent = load_2d_arr_from_file(load_file_in_mem());
-    for (int i = 0; filecontent[i] != NULL; i++) {
+    int power = 0;
+    int temp = 1;
+    for (int i = index; line[i] != '\0'; i++) {
+        if (line[i] < '0' || line[i] > '9')
+            break;
+        power += 1;
     }
-    printf("%d\n", result);
+    for (int i = index; line[i] != '\0'; i++) {
+        if (line[i] < '0' || line[i] > '9')
+            break;
+        for (int j = 0; j < power - 1; j++) {
+            temp *= 10;
+        }
+        result += (line[i] - '0') * temp;
+        temp = 0;
+    }
+    return result;
+}
+
+int main(void)
+{
+    char **filecontent = load_2d_arr_from_file(load_file_in_mem());
+    crates_t **orga = init_map(filecontent);
+    int num_stack = 0;
+    for (int i = 0; filecontent[7][i] != '\0'; i++)
+        if (filecontent[7][i] == '[')
+            num_stack += 1;
+        print_stacks(orga, num_stack);
+
+    for (int i = 10; filecontent[i] != NULL; i++) {
+        int value[3] = {0, 0, 0};
+        int index = 0;
+        for (int j = 0; filecontent[i][j] != '\0'; j++) {
+            if (filecontent[i][j] >= '0' && filecontent[i][j] <= '9') {
+                value[index] += get_value(filecontent[i], j);
+                if (filecontent[i][j + 1] < '0' || filecontent[i][j + 1] > '9')
+                    index++;
+            }
+        }
+        crates_t *temp_start = orga[value[1] - 1];
+        crates_t *temp_start2 = orga[value[1] - 1];
+        for (int j = 0; j < value[0] - 1; j++) {
+            temp_start = temp_start->next;
+            temp_start2 = temp_start2->next;
+        }
+        temp_start = temp_start->next;
+        temp_start2->next = orga[value[2] - 1];
+        orga[value[2] - 1] = orga[value[1] - 1];
+        orga[value[1] - 1] = temp_start;
+        print_stacks(orga, num_stack);
+    }
+    for (int i = 0; orga[i] != NULL; i++)
+        printf("%c", orga[i]->crate_name);
+    for (int i = 0; orga[i] != NULL; i++)
+        free_stack(orga[i]);
+    free(orga);
     for (int i = 0; filecontent[i] != NULL; i++)
         free(filecontent[i]);
     free(filecontent);
