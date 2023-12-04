a327d081310aab7dfb99f1a4bc0e3caf532d468f
karu <karu@Epitech-ubuntu.myguest.virtualbox.org>
2023-12-04 10:56:56 +0100

    {ADD}
    Adding puzzles of 3rd day

--- 2023/Day3/puzzle1/main.c
+++ 2023/Day3/puzzle1/main.c
@@ -65,11 +65,50 @@ char *load_file_in_mem(void)
     return content;
 }
 
+int get_number(char *line, int l)
+{
+    int result = 0;
+    int power = 1;
+    int i = l;
+    for (i; line[i] != '\0'; i++) {
+        if (!(line[i] >= '0' && line[i] <= '9'))
+            break;
+    }
+    for (int j = i - 1; j >= 0; j--) {
+        if (!(line[j] >= '0' && line[j] <= '9'))
+            break;
+        result += (line[j] - '0') * power;
+        power *= 10;
+    }
+    return result;
+}
+
 int main(void)
 {
     int result = 0;
     char **filecontent = load_2d_arr_from_file(load_file_in_mem());
     for (int i = 0; filecontent[i] != NULL; i++) {
+        for (int j = 0; filecontent[i][j] != '\0'; j++) {
+            if ((filecontent[i][j] < '0' || filecontent[i][j] > '9') && filecontent[i][j] != '.') {
+                for (int k = i - 1; k < i + 2; k++) {
+                    if (k < 0 || filecontent[k] == NULL)
+                        continue;
+                    for (int l = j - 1; l < j + 2; l++) {
+                        if (l < 0 || filecontent[k][l] == '\0')
+                            continue;
+                        if (filecontent[k][l] >= '0' && filecontent[k][l] <= '9') {
+                            result += get_number(filecontent[k], l);
+                            while (filecontent[k][l] != '\0') {
+                                if (filecontent[k][l] >= '0' && filecontent[k][l] <= '9')
+                                    l++;
+                                else
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     printf("%d\n", result);
     for (int i = 0; filecontent[i] != NULL; i++)
