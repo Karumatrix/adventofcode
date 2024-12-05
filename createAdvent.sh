1b9da3043606908ae9abac668d5b8f333f6e359d
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-05 15:00:53 +0100

    fix: improve createAdvent.sh to create file with a template (only in Go for now)

--- createAdvent.sh
+++ createAdvent.sh
@@ -2,6 +2,8 @@
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <year> <file_extension>"
+    echo "file_extension must not contain a dot."
+    echo "For example, python files must have a 'py' extension"
     exit 1
 fi
 
@@ -13,8 +15,8 @@ mkdir -p "$YEAR"
 for i in $(seq -w 1 25); do
     DAY_FOLDER="$YEAR/Day$i"
     mkdir -p "$DAY_FOLDER"
-    touch "$DAY_FOLDER/part1$EXTENSION"
-    touch "$DAY_FOLDER/part2$EXTENSION"
+    cp "./template/$EXTENSION/template.$EXTENSION" "$DAY_FOLDER/part1.$EXTENSION"
+    cp "./template/$EXTENSION/template.$EXTENSION" "$DAY_FOLDER/part2.$EXTENSION"
 done
 
 echo "Advent of Code structure for $YEAR created with $EXTENSION files"
