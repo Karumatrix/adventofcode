ad5b3c5eaf01230f2f084c644786f34f405bf41e
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-06 08:15:27 +0100

    feat: adding Day06 of 2024

--- 2024/Day06/part1.go
+++ 2024/Day06/part1.go
@@ -32,8 +32,82 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+type Position struct {
+	X, Y int
+}
+
+func getGuardPos(file []string) Position {
+	for x, line := range file {
+		for y, letter := range line {
+			if letter == '^' {
+				return Position{x, y}
+			}
+		}
+	}
+	return Position{-1, -1}
+}
+
+func updateGuardPos(file []string, offsetX, offsetY int, guardPos Position) (Position, []string) {
+	direction := file[guardPos.X][guardPos.Y]
+	row := []rune(file[guardPos.X])
+	row[guardPos.Y] = 'X'
+	file[guardPos.X] = string(row)
+	if guardPos.X < 0 || guardPos.X >= len(file) || guardPos.Y < 0 || guardPos.Y >= len(file[0]) || guardPos.X + offsetX < 0 || guardPos.X +offsetX >= len(file[0]) || guardPos.Y + offsetY < 0 || guardPos.Y + offsetY >= len(file[0]){
+		return Position{-1, -1}, file
+	}
+	if file[guardPos.X + offsetX][guardPos.Y + offsetY] == '#' {
+		switch direction {
+		case '^':
+			direction = '>'
+			guardPos.Y++
+		case '>':
+			direction = 'v'
+			guardPos.X++
+		case '<':
+			direction = '^'
+			guardPos.X--
+		case 'v':
+			direction = '<'
+			guardPos.Y--
+		}
+	} else {
+		guardPos.X += offsetX
+		guardPos.Y += offsetY
+	}
+	row = []rune(file[guardPos.X])
+	row[guardPos.Y] = rune(direction)
+	file[guardPos.X] = string(row)
+	return guardPos, file
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	guardPos := getGuardPos(file)
+
+	for {
+		if guardPos.X == -1 || guardPos.Y == -1 {
+			break
+		}
+		switch file[guardPos.X][guardPos.Y] {
+		case '^':
+			guardPos, file = updateGuardPos(file, -1, 0, guardPos)
+		case '>':
+			guardPos, file = updateGuardPos(file, 0, 1, guardPos)
+		case '<':
+			guardPos, file = updateGuardPos(file, 0, -1, guardPos)
+		case 'v':
+			guardPos, file = updateGuardPos(file, 1, 0, guardPos)
+		}
+	}
+	result := 0
+	for _, line := range file {
+		for _, letter := range line {
+			if letter == 'X' {
+				result++
+			}
+		}
+	}
+	fmt.Println(result)
 }
 
 func main() {
--- 2024/Day06/part2.go
+++ 2024/Day06/part2.go
@@ -32,8 +32,108 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+type Position struct {
+	X, Y int
+}
+
+func getGuardPos(file []string) Position {
+	for x, line := range file {
+		for y, letter := range line {
+			if letter == '^' {
+				return Position{x, y}
+			}
+		}
+	}
+	return Position{-1, -1}
+}
+
+func updateGuardPos(file []string, offsetX, offsetY int, guardPos Position) (Position, []string) {
+	direction := file[guardPos.X][guardPos.Y]
+	row := []rune(file[guardPos.X])
+	row[guardPos.Y] = 'X'
+	file[guardPos.X] = string(row)
+	if guardPos.X < 0 || guardPos.X >= len(file) || guardPos.Y < 0 || guardPos.Y >= len(file[0]) || guardPos.X + offsetX < 0 || guardPos.X +offsetX >= len(file[0]) || guardPos.Y + offsetY < 0 || guardPos.Y + offsetY >= len(file[0]){
+		return Position{-1, -1}, file
+	}
+	if file[guardPos.X + offsetX][guardPos.Y + offsetY] == '#' || file[guardPos.X + offsetX][guardPos.Y + offsetY] == 'O' {
+		switch direction {
+		case '^':
+			direction = '>'
+		case '>':
+			direction = 'v'
+		case '<':
+			direction = '^'
+		case 'v':
+			direction = '<'
+		}
+	} else {
+		guardPos.X += offsetX
+		guardPos.Y += offsetY
+	}
+	row = []rune(file[guardPos.X])
+	row[guardPos.Y] = rune(direction)
+	file[guardPos.X] = string(row)
+	return guardPos, file
+}
+
+func copyFile(file []string) []string {
+    copy := make([]string, len(file))
+    for i := range file {
+        copy[i] = file[i]
+    }
+    return copy
+}
+
+func testMap(guardPos Position, file []string) bool {
+	copyFile := copyFile(file)
+	directionsCheck := make(map[Position]string)
+	directionsCheck[guardPos] = string(copyFile[guardPos.X][guardPos.Y])
+	for {
+		switch copyFile[guardPos.X][guardPos.Y] {
+		case '^':
+			guardPos, copyFile = updateGuardPos(copyFile, -1, 0, guardPos)
+		case '>':
+			guardPos, copyFile = updateGuardPos(copyFile, 0, 1, guardPos)
+		case '<':
+			guardPos, copyFile = updateGuardPos(copyFile, 0, -1, guardPos)
+		case 'v':
+			guardPos, copyFile = updateGuardPos(copyFile, 1, 0, guardPos)
+		}
+		if guardPos.X == -1 || guardPos.Y == -1 {
+			break
+		}
+		for _, dir := range directionsCheck[guardPos] {
+			if dir == rune(copyFile[guardPos.X][guardPos.Y]) {
+				return true
+			}
+		}
+		directionsCheck[guardPos] += string(copyFile[guardPos.X][guardPos.Y])
+	}
+	return false
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	originalGuardPos := getGuardPos(file)
+	result := 0
+
+	for indexX, line := range file {
+		for indexY, _ := range line {
+			if file[indexX][indexY] == '.' {
+				fmt.Println("Testing:", indexX, indexY)
+				row := []rune(file[indexX])
+				row[indexY] = 'O'
+				file[indexX] = string(row)
+				if testMap(originalGuardPos, file) {
+					result++
+				}
+				row = []rune(file[indexX])
+				row[indexY] = '.'
+				file[indexX] = string(row)
+			}
+		}
+	}
+	fmt.Println(result)
 }
 
 func main() {
