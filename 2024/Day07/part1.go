916618c3e1f15117d97d7dbc50411acf9dc6bd77
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-07 06:29:06 +0100

    feat: adding Day07 of 2024

--- 2024/Day07/part1.go
+++ 2024/Day07/part1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func filepathToString(filepath string) string {
@@ -32,8 +33,39 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+func checkOperations(values []int, resultWanted, currValue, index int) bool {
+	if index == len(values) {
+		return currValue == resultWanted
+	}
+	if checkOperation(values, resultWanted, currValue + values[index], index + 1) {
+		return true
+	}
+	if checkOperation(values, resultWanted, currValue * values[index], index + 1) {
+		return true
+	}
+	return false
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	result := 0
+
+	for _, line := range file {
+		parts := strings.Split(line, ":")
+		resultWanted := 0
+		tmpValue := 0
+		fmt.Sscanf(parts[0], "%d", &resultWanted)
+		var values []int
+		subParts := strings.Split(parts[1], " ")
+		for _, part := range subParts {
+			fmt.Sscanf(part, "%d", &tmpValue)
+			values = append(values, tmpValue)
+		}
+		if checkOperations(values, resultWanted, values[0], 1) {
+			result += resultWanted
+		}
+	}
+	fmt.Println(result)
 }
 
 func main() {
--- 2024/Day07/part2.go
+++ 2024/Day07/part2.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func filepathToString(filepath string) string {
@@ -32,8 +34,49 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+func concatNumbers(nb1, nb2 int) (result int) {
+	nbStr1, nbStr2 := strconv.Itoa(nb1), strconv.Itoa(nb2)
+
+	result, _ = strconv.Atoi(nbStr1 + nbStr2)
+	return result
+}
+
+func checkOperations(values []int, resultWanted, currValue, index int) bool {
+	if index == len(values) {
+		return currValue == resultWanted
+	}
+	if checkOperation(values, resultWanted, currValue + values[index], index + 1) {
+		return true
+	}
+	if checkOperation(values, resultWanted, currValue * values[index], index + 1) {
+		return true
+	}
+	if checkOperation(values, resultWanted, concatNumbers(currValue, values[index]), index + 1) {
+		return true
+	}
+	return false
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	result := 0
+
+	for _, line := range file {
+		parts := strings.Split(line, ":")
+		resultWanted := 0
+		tmpValue := 0
+		fmt.Sscanf(parts[0], "%d", &resultWanted)
+		var values []int
+		subParts := strings.Split(parts[1], " ")
+		for _, part := range subParts {
+			fmt.Sscanf(part, "%d", &tmpValue)
+			values = append(values, tmpValue)
+		}
+		if checkOperations(values, resultWanted, values[0], 1) {
+			result += resultWanted
+		}
+	}
+	fmt.Println(result)
 }
 
 func main() {
