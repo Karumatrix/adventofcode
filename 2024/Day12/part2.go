3ce4caffa0132e14ba0a1d56eaff9608826dec2e
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-13 11:10:29 +0100

    fix: adding new Day12 of 2024

--- 2024/Day12/part2.go
+++ 2024/Day12/part2.go
@@ -6,168 +6,127 @@ import (
 	"os"
 )
 
-type Limits struct {
-	Top, Bottom, Left, Right int
+func filepathToString(filepath string) string {
+    content, err := os.ReadFile(filepath)
+    if err != nil {
+		panic(err)
+    }
+    return string(content)
+}
+
+func filepathToStringArray(filepath string) []string {
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return lines
 }
 
 type Position struct {
 	X, Y int
 }
 
-func readInput(fileName string) [][]byte {
-	file, err := os.Open(fileName)
-	if err != nil {
-		panic(err)
+func getSides(file []string, rows, cols int, cases map[Position]bool, target byte) (sides int) {
+	interiorAngles := 0
+	directions := []Position{
+		{-1, 0},{1, 0},{0, -1},{0, 1},
 	}
-	defer file.Close()
+	checked := make(map[Position]int)
+	for pos := range cases {
+		nbNeighbour := 0
+		for _, dir := range directions {
+			if pos.X + dir.X < 0 || pos.X + dir.X >= len(file) {
+				nbNeighbour++
+			}
+			if pos.Y + dir.Y < 0 || pos.Y + dir.Y >= len(file[0]) {
+				nbNeighbour++
+			}
+			if pos.X + dir.X >= 0 && pos.X + dir.X < len(file) && pos.Y + dir.Y >= 0 && pos.Y + dir.Y < len(file[0]) && file[pos.X + dir.X][pos.Y + dir.Y] != target {
+				nbNeighbour++
+				checked[Position{pos.X + dir.X, pos.Y + dir.Y}]++
+			}
+		}
 
-	var f [][]byte
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		f = append(f, []byte(line))
+		switch nbNeighbour {
+		case 2:
+			fmt.Print("+90")
+			interiorAngles += 90
+			break
+		case 3:
+			fmt.Print("+180")
+			interiorAngles += 180
+			break
+		}
 	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
+	for _, nb := range checked {
+		if nb == 2 {
+			fmt.Print("+270")
+			interiorAngles += 270
+		}
 	}
-	return f
+	fmt.Println()
+	sides = (interiorAngles / 180) + 2
+	return
 }
 
-func replace1WithDot(f [][]byte, b Limits) {
-	for z := b.Top; z <= b.Bottom; z++ {
-		for w := b.Left; w <= b.Right; w++ {
-			if f[z][w] == '1' {
-				f[z][w] = '.'
+func getArea(file []string, x, y, rows, cols int, region byte, checked [][]bool, cases map[Position]bool) (area int) {
+	if x < 0 || y < 0 || x >= rows || y >= cols || file[x][y] != region {
+		return 0
+	}
+	if checked[x][y] {
+		return 0
+	}
+	checked[x][y] = true
+	cases[Position{x, y}] = true
+	area = 1
+	directions := []Position{
+		{-1, 0},{1, 0},{0, -1},{0, 1},
+	}
+	for _, dir := range directions {
+		area += getArea(file, x + dir.X, y + dir.Y, rows, cols, region, checked, cases)
+	}
+	return
+}
+
+func aoc(filepath string) {
+	file := filepathToStringArray(filepath)
+
+	rows := len(file)
+	cols := len(file[0])
+	checked := make([][]bool, rows)
+	for i := range checked {
+		checked[i] = make([]bool, cols)
+	}
+	result := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if !checked[i][j] {
+				cases := make(map[Position]bool)
+				area := getArea(file, i, j, rows, cols, file[i][j], checked, cases)
+				sides := getSides(file, rows, cols, cases, file[i][j])
+				fmt.Println(area, sides)
+				fmt.Println()
+				result += area * sides
 			}
 		}
 	}
-}
-
-func getSides(f [][]byte, limits Limits) [][][]int {
-	rows := limits.Bottom - limits.Top + 1
-	cols := limits.Right - limits.Left + 1
-
-	res := make([][][]int, 4)
-	for i := 0; i < 4; i++ {
-		if i < 2 {
-			res[i] = make([][]int, rows)
-		} else {
-			res[i] = make([][]int, cols)
-		}
-	}
-
-	for i := limits.Top; i <= limits.Bottom; i++ {
-		for j := limits.Left; j <= limits.Right; j++ {
-			if f[i][j] == '1' {
-				if j == 0 || f[i][j-1] != '1' {
-					res[0][i-limits.Top] = append(res[0][i-limits.Top], j)
-				}
-				if j+1 >= len(f[0]) || f[i][j+1] != '1' {
-					res[1][i-limits.Top] = append(res[1][i-limits.Top], j)
-				}
-				if i == 0 || f[i-1][j] != '1' {
-					res[2][j-limits.Left] = append(res[2][j-limits.Left], i)
-				}
-				if i+1 >= len(f) || f[i+1][j] != '1' {
-					res[3][j-limits.Left] = append(res[3][j-limits.Left], i)
-				}
-			}
-		}
-	}
-
-	return res
-}
-
-func getShape(f [][]byte, i, j int) (int, [][][]int) {
-	dirs := []Position{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
-	searched := f[i][j]
-	limits := Limits{i, i, j, j}
-	area := 0
-
-	var flood func(x, y int)
-	flood = func(x, y int) {
-		if x < 0 || x >= len(f) || y < 0 || y >= len(f[0]) || f[x][y] != searched {
-			return
-		}
-
-		if x < limits.Top {
-			limits.Top = x
-		}
-		if x > limits.Bottom {
-			limits.Bottom = x
-		}
-		if y < limits.Left {
-			limits.Left = y
-		}
-		if y > limits.Right {
-			limits.Right = y
-		}
-
-		f[x][y] = '1'
-		area++
-
-		for _, d := range dirs {
-			flood(x+d.X, y+d.Y)
-		}
-	}
-
-	flood(i, j)
-	sides := getSides(f, limits)
-	replace1WithDot(f, limits)
-
-	return area, sides
-}
-
-func getTotalSides(sides [][][]int) int {
-	tot := 0
-	for _, side := range sides {
-		for i := 0; i < len(side); i++ {
-			for len(side[i]) > 0 {
-				tot++
-				elt := side[i][0]
-				side[i] = side[i][1:]
-				j := i + 1
-				for j < len(side) {
-					found := false
-					for k, v := range side[j] {
-						if v == elt {
-							side[j] = append(side[j][:k], side[j][k+1:]...)
-							found = true
-							break
-						}
-					}
-					if !found {
-						break
-					}
-					j++
-				}
-			}
-		}
-	}
-	return tot
+	fmt.Println(result)
 }
 
 func main() {
-	f := readInput("input.txt")
-	score2 := 0
-
-	for i := 0; i < len(f); i++ {
-		for j := 0; j < len(f[0]); j++ {
-			if f[i][j] == '.' {
-				continue
-			}
-
-			area, sides := getShape(f, i, j)
-			circumference := 0
-			for _, side := range sides {
-				for _, val := range side {
-					circumference += len(val)
-				}
-			}
-
-			score2 += getTotalSides(sides) * area
-		}
+	if len(os.Args) < 2 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println("USAGE: go run <file> <input-file>")
+	} else {
+		aoc(os.Args[1])
 	}
-
-	fmt.Println(score2)
 }
