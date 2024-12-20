4811542015195376b4c27c8cbd56e1fe5394942d
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-20 16:29:46 +0100

    feat: adding Day20 of 2024

--- 2024/Day20/part1.go
+++ 2024/Day20/part1.go
@@ -32,8 +32,108 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+type Position struct {
+	X, Y int
+}
+
+func getMaze(file []string) (maze [][]int, start, end Position) {
+	rows := len(file)
+	cols := len(file[0])
+	maze = make([][]int, rows)
+	for indX, line := range file {
+		maze[indX] = make([]int, cols)
+		for indY, letter := range line {
+			switch letter {
+			case 'S':
+				start = Position{indX, indY}
+				maze[indX][indY] = -1
+			case 'E':
+				end = Position{indX, indY}
+				maze[indX][indY] = -2
+			case '.':
+				maze[indX][indY] = -3
+			case '#':
+				maze[indX][indY] = -4
+			}
+		}
+	}
+	return
+}
+
+func findPath(maze [][]int, start, end Position) int {
+	directions := []Position{
+		{0, 1}, {0, -1}, {1, 0}, {-1, 0},
+	}
+	score := 0
+
+	for {
+		if start.X == end.X && start.Y == end.Y {
+			break
+		}
+		maze[start.X][start.Y] = score
+		score++
+
+		for _, dir := range directions {
+			if maze[start.X + dir.X][start.Y + dir.Y] == -3 || maze[start.X + dir.X][start.Y + dir.Y] == -2 {
+				maze[start.X + dir.X][start.Y + dir.Y] = score
+				start.X += dir.X
+				start.Y += dir.Y
+				break
+			}
+		}
+	}
+	return score
+}
+
+func abs(nb int) int {
+	if nb < 0 {
+		return -nb
+	}
+	return nb
+}
+
+func getScore(maze [][]int, size int) int {
+	directions := make(map[Position]bool)
+	for i := 0; i < size; i++ {
+		for j := 0; j < size - i; j++ {
+			directions[Position{i, j}] = true
+			directions[Position{i, -j}] = true
+			directions[Position{-i, j}] = true
+			directions[Position{-i, -j}] = true
+		}
+	}
+	score := 0
+	for x, line := range maze {
+		for y, tmp := range line {
+			if tmp == -4 {
+				continue
+			}
+			for dir := range directions {
+				tmpX, tmpY := x + dir.X, y + dir.Y
+				if tmpX < 0 || tmpX >= len(maze) || tmpY < 0 || tmpY >= len(maze[0]) {
+					continue
+				}
+				if maze[tmpX][tmpY] == -4 {
+					continue
+				}
+				if tmp > maze[tmpX][tmpY] {
+					continue
+				}
+				dist := maze[tmpX][tmpY] - tmp - abs(dir.X) - abs(dir.Y)
+				if dist >= 100 {
+					score++
+				}
+			}
+		}
+	}
+	return score
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	maze, start, end := getMaze(file)
+	findPath(maze, start, end)
+	fmt.Println(getScore(maze, 3))
 }
 
 func main() {
