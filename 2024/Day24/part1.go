1561a2ced169c8a94c91de06b94e14fab50ee027
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-24 13:10:13 +0100

    feat: adding days

--- 2024/Day24/part1.go
+++ 2024/Day24/part1.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func filepathToString(filepath string) string {
@@ -32,8 +35,65 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+func reverseStr(str string) (string) {
+	bytes := []byte(str)
+	for i, j := 0, len(bytes) - 1; i < j; i, j = i+1, j-1 {
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+	}
+	return string(bytes)
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	index := 0
+	wires := make(map[string]uint8)
+
+	for file[index] != "" {
+		parts := strings.Split(file[index], " ")
+		value, _ := strconv.Atoi(parts[1])
+		wires[parts[0][:len(parts[0]) -1]] = uint8(value)
+		index++
+	}
+	index++
+	file = file[index:]
+	for len(file) > 0 {
+		newFile := []string{}
+		for _, line := range file {
+			parts := strings.Split(line, " ")
+			a, b, dest := parts[0], parts[2], parts[4]
+			op := parts[1]
+
+			_, existsA := wires[a]
+			_, existsB := wires[b]
+			if existsA && existsB {
+				switch op {
+				case "AND":
+					wires[dest] = wires[a] & wires[b]
+				case "OR":
+					wires[dest] = wires[a] | wires[b]
+				case "XOR":
+					wires[dest] = wires[a] ^ wires[b]
+				}
+			} else {
+				newFile = append(newFile, line)
+			}
+		}
+		file = newFile
+	}
+	keys := make([]string, 0, len(wires))
+	for key := range wires {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	binary := ""
+	for _, key := range keys {
+		if key[0] == 'z' {
+			binary += strconv.Itoa(int(wires[key]))
+		}
+	}
+	binary = reverseStr(binary)
+	result, _ := strconv.ParseInt(binary, 2, 64)
+	fmt.Println(result)
 }
 
 func main() {
--- 2024/Day24/part2.go
+++ 2024/Day24/part2.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func filepathToString(filepath string) string {
@@ -32,8 +35,70 @@ func filepathToStringArray(filepath string) []string {
 	return lines
 }
 
+func findHighestZ(file []string) string {
+	result := "z00"
+
+	for _, line := range file {
+		parts := strings.Split(line, " ")
+		if parts[4][0] == 'z' {
+			curr, _ := strconv.Atoi(parts[4][1:])
+			ref, _ := strconv.Atoi(result[1:])
+			if curr > ref {
+				result = parts[4]
+			}
+		}
+	}
+	return result
+}
+
 func aoc(filepath string) {
-	fmt.Println("Hello world")
+	file := filepathToStringArray(filepath)
+	index := 0
+	wires := make(map[string]uint8)
+	for file[index] != "" {
+		parts := strings.Split(file[index], " ")
+		value, _ := strconv.Atoi(parts[1])
+		wires[parts[0][:len(parts[0]) -1]] = uint8(value)
+		index++
+	}
+	index++
+	file = file[index:]
+	highestZ := findHighestZ(file)
+	swaps := make(map[string]bool)
+	for _, line := range file {
+		parts := strings.Split(line, " ")
+		a, op, b, dest := parts[0], parts[1], parts[2], parts[4]
+		if dest[0] == 'z' && op != "XOR" && dest != highestZ  {
+			swaps[dest] = true
+		}
+		if op == "XOR" && dest[0] != 'x' && dest[0] != 'y' && dest[0] != 'z' && a[0] != 'x' && a[0] != 'y' && a[0] != 'z' && b[0] != 'x' && b[0] != 'y' && b[0] != 'z' {
+			swaps[dest] = true
+		}
+		if op == "AND" && a != "x00" && b != "x00" {
+			for _, subLine := range file {
+				subParts := strings.Split(subLine, " ")
+				subA, subOp, subB := subParts[0], subParts[1], subParts[2]
+				if (dest == subA || dest == subB) && subOp != "OR" {
+					swaps[dest] = true
+				}
+			}
+		}
+		if op == "XOR" {
+			for _, subLine := range file {
+				subParts := strings.Split(subLine, " ")
+				subA, subOp, subB := subParts[0], subParts[1], subParts[2]
+				if (dest == subA || dest == subB) && subOp == "OR" {
+					swaps[dest] = true
+				}
+			}
+		}
+	}
+	keys := make([]string, 0, len(swaps))
+	for key := range swaps {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	fmt.Println(strings.Join(keys, ","))
 }
 
 func main() {
